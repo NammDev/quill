8da541b440311a2124da1aa478762f1bad5021cd
Nam <namdeveloper.ca@gmail.com>
2023-10-05 21:07:25 +0700

    kinde auth

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,9 +3,12 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import { ArrowRight } from 'lucide-react'
 import { buttonVariants } from '../ui/button'
 import MobileNav from './MobileNav'
+import UserAccountNav from './UserAccountNav'
+import { LoginLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 const Navbar = () => {
-  const user = false
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
 
   return (
     <nav className='sticky inset-x-0 top-0 z-30 w-full transition-all border-b border-gray-200 h-14 bg-white/75 backdrop-blur-lg'>
@@ -14,10 +17,59 @@ const Navbar = () => {
           <Link href='/' className='z-40 flex font-semibold'>
             <span>quill.</span>
           </Link>
-
           <MobileNav isAuth={!!user} />
+          <div className='items-center hidden space-x-4 sm:flex'>
+            {!user ? (
+              <>
+                <Link
+                  href='/pricing'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}
+                >
+                  Sign in
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: 'sm',
+                  })}
+                >
+                  Get started <ArrowRight className='ml-1.5 h-5 w-5' />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  href='/dashboard'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                    size: 'sm',
+                  })}
+                >
+                  Dashboard
+                </Link>
 
-          <div className='items-center hidden space-x-4 sm:flex'></div>
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? 'Your Account'
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ''}
+                  imageUrl={user.picture ?? ''}
+                />
+              </>
+            )}
+          </div>{' '}
         </div>
       </MaxWidthWrapper>
     </nav>
---
+++ components/layout/UserAccountNav.tsx
@@ -0,0 +1,78 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Gem } from 'lucide-react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
+import { Button } from '../ui/button'
+import { Avatar, AvatarFallback } from '../ui/avatar'
+import { Icons } from '../shared/Icon'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/server'
+
+interface UserAccountNavProps {
+  email: string | undefined
+  name: string
+  imageUrl: string
+}
+
+const UserAccountNav = async ({ email, imageUrl, name }: UserAccountNavProps) => {
+  //   const subscriptionPlan = await getUserSubscriptionPlan()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='overflow-visible'>
+        <Button className='w-8 h-8 rounded-full aspect-square bg-slate-400'>
+          <Avatar className='relative w-8 h-8'>
+            {imageUrl ? (
+              <div className='relative w-full h-full aspect-square'>
+                <Image fill src={imageUrl} alt='profile picture' referrerPolicy='no-referrer' />
+              </div>
+            ) : (
+              <AvatarFallback>
+                <span className='sr-only'>{name}</span>
+                <Icons.user className='w-4 h-4 text-zinc-900' />
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className='bg-white' align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-0.5 leading-none'>
+            {name && <p className='text-sm font-medium text-black'>{name}</p>}
+            {email && <p className='w-[200px] truncate text-xs text-zinc-700'>{email}</p>}
+          </div>
+        </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href='/dashboard'>Dashboard</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          {/* {subscriptionPlan?.isSubscribed ? (
+            <Link href='/dashboard/billing'>Manage Subscription</Link>
+          ) : (
+            <Link href='/pricing'>
+              Upgrade <Gem className='text-blue-600 h-4 w-4 ml-1.5' />
+            </Link>
+          )} */}
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className='cursor-pointer'>
+          <LogoutLink>Log out</LogoutLink>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserAccountNav
