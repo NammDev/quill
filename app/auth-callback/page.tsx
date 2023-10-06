9ddd005a4b1ce1aefdd389ffff097b27139aa6a3
Nam <namdeveloper.ca@gmail.com>
2023-10-06 13:38:57 +0700

    Test using tRpc

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -2,13 +2,22 @@
 
 import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
+import { trpc } from '../_trpc/client'
 
 const Page = () => {
-  const route = useRouter()
+  const router = useRouter()
   const searchParams = useSearchParams()
   const origin = searchParams.get('origin')
 
   // trpc
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess({ success }) {
+      if (success) {
+        // user is sync to db
+        router.push(origin ? `/${origin}` : '/dashboard')
+      }
+    },
+  })
   return <div>page</div>
 }
 
--- trpc/index.ts
+++ trpc/index.ts
@@ -1,6 +1,16 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { publicProcedure, router } from './trpc'
-export const appRouter = router({})
+import { TRPCError } from '@trpc/server'
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession()
+    const user = getUser()
+    if (!user.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+    // check if user is in the database
+    return { success: true }
+  }),
+})
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter
