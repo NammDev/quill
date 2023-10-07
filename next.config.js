fd1bfab4573545fc8249afabb077c29abfd05d88
Nam <namdeveloper.ca@gmail.com>
2023-10-07 22:03:05 +0700

    redirect to kinde api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
+      },
+      {
+        source: '/sign-up',
+        destination: '/api/auth/register',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
