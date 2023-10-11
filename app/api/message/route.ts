821d444a7670427dce5d70f176ba19756af37c3f
Nam <namdeveloper.ca@gmail.com>
2023-10-11 16:52:57 +0700

    Context Provider & backend: add message to db

---
+++ app/api/message/route.ts
@@ -0,0 +1,23 @@
+import { db } from '@/db'
+import { SendMessageValidator } from '@/lib/validators/SendMessageValidator'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from 'next/server'
+
+export const POST = async (req: NextRequest) => {
+  // endpoint for asking question to a pdf file
+  const body = await req.json()
+
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+  const { id: userId } = user
+  if (!userId) return new Response('Unauthorized', { status: 401 })
+
+  const { fileId, message } = SendMessageValidator.parse(body)
+  const file = await db.file.findFirst({ where: { id: fileId, userId } })
+  if (!file) return new Response('Not Found', { status: 404 })
+
+  // add question to database
+  await db.message.create({ data: { text: message, isUserMessage: true, userId, fileId } })
+
+  // answer question
+}
